087a3a157c934a080cb366e4933964796cdfa917
YuseiEleven <yusei.eleven@gmail.com>
2018-09-07 22:05:23 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,14 +8,9 @@ const client = new Client({ disableEveryone: true });
 
 client.on('ready', () => {
 	console.log('Bot en ligne!');
-  client.user.setActivity('🏰 !temps')});
-  
-  
-  
+  client.user.setActivity('ðŸ° !temps')});
 client.on('warn', console.warn);
 client.on('error', console.error);
-
-
 client.on('message', message => {
   if (message.content === prefix + 'ping') {
     const then = Date.now();
@@ -31,9 +26,8 @@ client.on('message', message => {
   ));
   }
 });
-
 client.on('guildMemberAdd', member => {
-  var channel = client.channels.get('387188744207335425');
+  var channel = client.channels.get('487657315579854848');
   if (!channel) return;
   channel.send(ce(
     "#00FF00", {"name": member.guild.name, "icon_url": member.guild.iconURL}, "", "",
@@ -42,35 +36,33 @@ client.on('guildMemberAdd', member => {
     {"thumbnail": member.user.displayAvatarURL}, true
   ));
 });
-
 client.on('guildMemberRemove', member => {
-  var channel = client.channels.get('387188744207335425');
+  var channel = client.channels.get('487657315579854848');
   if (!channel) return;
   channel.send(ce(
     "#FF0000", {"name": member.guild.name, "icon_url": member.guild.iconURL}, "", "",
-    [{"name": "A bientôt!", "value": member.user.username }],
+    [{"name": "A bientÃ´t!", "value": member.user.username }],
     {"text": "", "icon_url": member.guild.iconURL}, 
     {"thumbnail": member.user.displayAvatarURL}, true
   ));
 });
-
 client.on('guildBanAdd', (guild, user) => {
-  var channel = client.channels.get('387188744207335425');
+  var channel = client.channels.get('487657315579854848');
   if (!channel) return;
   channel.send(ce(
     "#010101", {"name": guild.name, "icon_url": guild.iconURL}, "", "",
-    [{"name": "Ce joueur vient d'être banni!", "value": user.username }],
+    [{"name": "Ce joueur vient d'Ãªtre banni!", "value": user.username }],
     {"text": "", "icon_url": guild.iconURL}, 
     {"thumbnail": user.displayAvatarURL}, true
   ));
 });
 
 client.on('guildBanRemove', (guild, user) => {
-  var channel = client.channels.get('387188744207335425');
+  var channel = client.channels.get('487657315579854848');
   if (!channel) return;
   channel.send(ce(
     "#EE82EE", {"name": guild.name, "icon_url": guild.iconURL}, "", "",
-    [{"name": "Ce joueur est désormais débanni", "value": user.username }],
+    [{"name": "Ce joueur est dÃ©sormais dÃ©banni", "value": user.username }],
     {"text": "", "icon_url": guild.iconURL}, 
     {"thumbnail": user.displayAvatarURL}, true
   ));
@@ -84,39 +76,45 @@ for (var park in Themeparks.Parks)
 // access a specific park
 var disneyMagicKingdom = new Themeparks.Parks.DisneylandParisMagicKingdom();
 
-// access wait times by Promise
 client.on('message', message => {
-	if (message.content === prefix + 'temps') {
-    var channel = client.channels.get('487596220479438848');
-		if (!channel) return;
-    channel.send("Disneyland Paris - temps d'attentes:")
-    disneyMagicKingdom.GetWaitTimes().then(function(rides) {
-			// print each wait time
+  if (message.content === prefix + 'fastPass') {
+    disneyMagicKingdom.GetRideObject().then(function(fastPass) {
+      // print each wait time
       for(var i=0, ride; ride=rides[i++];) {
-        channel.send(ride.name + ": " + ride.waitTime + " minutes.");
-        channel.send("-------------------")
+        var channel = client.channels.get('487596220479438848');
+        if (!channel) return;
+        channel.send(fastPass.Available);
+        channel.send("===============================")
       }
-      const chateau = client.emojis.find("name", "chateau")
-      channel.send(`${chateau} Bonne séjour à Disneyland Paris!`)
     }, console.error)}});
-    
-    client.on('message', message => {
-      if (message.content === prefix + 'fastpass') {
-        disneyMagicKingdom.GetRideObject().then(function(FastPass) {
-          // print each wait time
-          for(var i=0, ride; ride=rides[i++];) {
-            var channel = client.channels.get('487596220479438848');
-            if (!channel) return;
-            channel.send(fastPass.Available);
-            channel.send("===============================")
-          }
-        }, console.error)}});
-
-
 client.on('message', message => {
-  if (message.content === prefix + 'blabla') {
+  if (message.content === prefix + 'debug') {
     var channel = client.channels.get('487596220479438848');
     if (!channel) return;
     const chateau = client.emojis.find("name", "chateau")
     channel.send(`${chateau}`);
   }});
+// access wait times by Promise
+client.on('message', message => {
+  if (message.content === prefix + 'temps') {
+    var channel = client.channels.get('487596220479438848');
+    if (!channel) return;
+    const chateau = client.emojis.find("name", "chateau")
+    channel.send(ce(
+      "#010101", {"name": "", "icon_url": ""}, "", "",
+      [{"name": `${chateau} Disneyland Paris`, "value": "Temps d'attentes"}],
+      {"text": "", "icon_url": ""}, 
+      {"thumbnail": "", "image": ""}, false
+    ));
+    disneyMagicKingdom.GetWaitTimes().then(function(rides) {
+    // print each wait time
+    for(var i=0, ride; ride=rides[i++];) {
+    var channel = client.channels.get('487596220479438848');
+    if (!channel) return;
+    channel.send(ce(
+    "#010101", {"name": "", "icon_url": ""}, "", "",
+    [{"name": ride.name, "value": ride.waitTime + " minutes."}],
+    {"text": "", "icon_url": ""}, 
+    {"thumbnail": "", "image": ""}, true
+    ))}});
+}});
