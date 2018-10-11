5264e651b9df6678fb17ff43cd2dd7ae4b0e53e1
YuseiEleven <yusei.eleven@gmail.com>
2018-10-11 19:42:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,12 +5,13 @@ var prefix = "!";
 const { Client, Util } = require('discord.js');
 const PREFIX = "!";
 const client = new Client({ disableEveryone: true });
+var opus = require('opusscript')
 client.login(process.env.DISNEY);
 
 // Lancement
 client.on('ready', () => {
 	console.log('Bot en ligne!');
-  client.user.setActivity('🏰 !help')});
+  client.user.setActivity('🏰 !aide')});
 client.on('warn', console.warn);
 client.on('error', console.error);
 
@@ -131,7 +132,7 @@ client.on('message', message => {
     ))}});
 }});
 
-// Utilitaire
+//Modération
 client.on('message', async message => {
   if(message.author.bot) return;
   if(message.content.indexOf(PREFIX) !== 0) return;
@@ -149,7 +150,6 @@ client.on('message', async message => {
 });
 var bannedwords = "pute,p ute,p u t e,pu te,put e,putain,p utain,pu tain,put ain,puta in,putai n,putaclic,clickbait,click bait,attention whore,,whore,fdp, fils de put,fille de pute,fils de pute,fils de puterie,fille de p ute,fils de p ute,f d p,fd p,f dp,bordel,catin,schizo,schizophrène,schiso,drogué,droguée,gouine,goudou,pédé,pd,p d,,enculé,enculer,enculée,enculés,enculées,tantouze,tafiole,tapette,con,c on,co n,c onne,co nne,con ne,conn e,conne,salope,s alope,salop,salo,allumeuse,chaudasse,mégère,pimbêche,connasse,connard,pétasse,niquer,nique,poufiasse,pisseuse,couillon,bite,nazi,beurette,rebeu,travelo,transsexuel,trans,hermaphrodite,casos,ksos,clodo,plouc,anal,anus,ass,a s s,a ss,bitch,b itch,bastard,boob,butt,blowjob,blow job,buttplug,clitoris,cock,crap,dick,dildo,fuck,f u c k,penis,pussy,scrotum,sex,s e x,sexe,s e x e,s ex,se x,slut,vagina".split(",");
 
-//test
 client.on("message", msg => {
   if (msg.guild === null) return;
   if (!msg.content.toLowerCase().startsWith(prefix)) return;
@@ -189,6 +189,7 @@ client.on("message", msg => {
     var mc = msg.content.split(" ")[2];
     if (!mem)
       return msg.reply('Veuillez mentionner un utilisateur. (!mute @test#1234 test)');
+    if (mem.hasPermission("MUTE_MEMBERS")) return;
     if (!mc)
       return msg.reply('Veuillez ajouter une raison. (!mute @test#1234 test)');
     if (msg.guild.roles.find("name", "Muet")) {
@@ -217,3 +218,50 @@ client.on("message", msg => {
     }
   }
 });
+
+//Utilitaire
+client.on('message', message => {
+  if (message.content === prefix + 'aide') {
+    message.channel.send(ce(
+      "#010101", {"name": `Aide`, "icon_url": ""}, "", "",
+      [{"name": "!disneyland", "value": "Afficher les temps d'attentes du parc Disneyland Paris."},
+      {"name": "!studios", "value": "Afficher les temps d'attentes du parc Walt Disney Studios."},
+      {"name": "!castmember", "value": "Afficher les commandes de modération."}],
+      {"text": "", "icon_url": ""}, 
+      {"thumbnail": "", "image": ""}, true
+    ))
+  }
+});
+client.on('message', message => {
+  if (message.content === prefix + 'castmember') {
+    if (!message.member.hasPermission("MUTE_MEMBERS")) return;
+    message.channel.send(ce(
+      "#010101", {"name": `Aide CastMember`, "icon_url": ""}, "", "",
+      [{"name": "!purge <2-100>", "value": "Supprimer des messages dans un salon textuel."},
+      {"name": "!kick <@guest#1234> <raison>", "value": "Expulser un guest du serveur discord."},
+      {"name": "!ban <@guest#1234> <raison>", "value": "Bannir un guest du serveur discord."},
+      {"name": "!mute <@guest#1234> <raison>", "value": "Rendre muet un guest."},
+      {"name": "!unmute <@guest#1234>", "value": "Redonner la parole à un guest."},
+      {"name": "!sondage;<Question>;<Choix1>;<Choix2>;<Choix3>", "value": "Lancer un sondage."}],
+      {"text": "", "icon_url": ""}, 
+      {"thumbnail": "", "image": ""}, true
+    ))
+  }
+});
+
+client.on('message', message => {
+  const args = message.content.slice(prefix.length).trim().split(';');
+  const command = args.shift().toLowerCase();
+  if (command === "sondage") {
+    if(!message.member.hasPermission("KICK_MEMBERS")) return;
+    let question = args[0];
+    let choix1 = args[1];
+    let choix2 = args[2];
+    let choix3 = args[3];
+    if (args.length === 0)
+      return message.reply('**Mauvais format:** `!sondage;<Question>;<Choix1>;<Choix2>;<Choix3>`')
+  var channel = client.channels.get('499918358263889920');
+  channel.send("~~-----------~~" + '\n' + '\n' + ":question:" + `**__${question}__**` + '\n'  + '\n' + "" + ":one:" + ` **${choix1}**` +  '\n' + ":two:" + ` **${choix2}**` +  '\n' + ":three:" + ` **${choix3}**` + '\n' + '\n' + "*(Créer par: " + message.author.username + ")*")
+  .then(function (message) {
+    message.react('1⃣').then(() => message.react('2⃣')).then(() => message.react('3⃣'));
+    })}});
